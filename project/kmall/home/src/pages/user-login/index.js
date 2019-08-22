64afd643198ee8fe11d0399d4f3d50bd6a2cbb73
zzzuowenbo <2895732099@qq.com>
2019-08-22 14:32:44 +0800

    '822'

--- project/kmall/home/src/pages/user-login/index.js
+++ project/kmall/home/src/pages/user-login/index.js
@@ -1,3 +1,27 @@
 require('pages/common/logo');
 require('pages/common/footer');
 require('./index.css');
+
+var page = {
+	init:function(){
+		this.bindEvent()
+	},
+	bindEvent:function(){
+		var _this = this;
+		$('#btn-submit').on('click',function(){
+			_this.submit()
+		});
+		$('input').on('keyup',function(ev){
+            if(ev.keyCode == 13){
+                _this.submit()
+            }
+        })
+	},
+	submit:function(){
+		alert('111')
+	}
+}
+
+$(function(){
+	page.init()
+})
