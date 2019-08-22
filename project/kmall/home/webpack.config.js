dde47294ee5f607ad1a4244f2e37d2cc4d1f6c8e
zzzuowenbo <2895732099@qq.com>
2019-08-22 09:18:46 +0800

    '822'

--- project/kmall/home/webpack.config.js
+++ project/kmall/home/webpack.config.js
@@ -3,6 +3,13 @@ const htmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
+const getHtmlConfig = (name)=>({
+    template:'./src/views/'+name+'.html',
+    filename:name+'.html',
+    hash:true,
+    chunks:['common',name]
+})
+
 module.exports = {
     //指定环境
     mode:'development',
@@ -10,7 +17,7 @@ module.exports = {
     entry:{
         common:'./src/pages/common/index.js',
         index:'./src/pages/index/index.js',
-        list:'./src/pages/list/index.js',
+        list:'./src/pages/list/index.js'
     },
     //出口
     output: {
@@ -31,17 +38,17 @@ module.exports = {
     module: {
         rules: [
         //处理css文件
-          {
-            test: /\.css$/,
-            use: [
-              {
-                loader: MiniCssExtractPlugin.loader,
-                options: {
-                }
-              },
-              "css-loader"
-            ]
-          },       
+            {
+                test: /\.css$/,
+                use: [
+                    {
+                        loader: MiniCssExtractPlugin.loader,
+                        options: {
+                        }
+                    },
+                    "css-loader"
+                ]
+            },       
         //处理图片
             {
                 test: /\.(png|jpg|gif)$/i,
@@ -69,18 +76,8 @@ module.exports = {
     },
     plugins:[
         new CleanWebpackPlugin(),
-        new htmlWebpackPlugin({
-            template:'./src/views/index.html',//模板文件
-            filename:'index.html',//输出的文件名
-            hash:true,//给生成的js/css文件添加一个唯一的hash
-            chunks:['common','index']
-        }),
-        new htmlWebpackPlugin({
-            template:'./src/views/list.html',//模板文件
-            filename:'list.html',//输出的文件名
-            hash:true,//给生成的js/css文件添加一个唯一的hash
-            chunks:['common','list']
-        }),        
+        new htmlWebpackPlugin(getHtmlConfig('index')),
+        new htmlWebpackPlugin(getHtmlConfig('list')),        
         new MiniCssExtractPlugin({
             filename: 'css/[name]-[hash]-bundle.css'
         })
@@ -89,5 +86,5 @@ module.exports = {
         contentBase: './dist',//内容的目录
         port:3001,//指定服务端口
         historyApiFallback:true//让h5路由不向后端发送请求
-    },                
+    }               
 }
