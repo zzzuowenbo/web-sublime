5f3462188df0eaed1e61283d62a291509a397876
zzzuowenbo <2895732099@qq.com>
2019-09-02 17:17:52 +0800

    add

--- js/js/009.js
+++ js/js/009.js
@@ -1,11 +1,10 @@
-
 window.onload=function(){
 	function toChange(){
-			var oBox=document.getElementById('box');
-			oBox.style.background='pink';
-	        oBox.style.width='50px';
-	        oBox.style.height='50px';
+		var oBox=document.getElementById('box');
+		oBox.style.background='pink';
+	    oBox.style.width='50px';
+	    oBox.style.height='50px';
     }
-var oBox=document.getElementById('box');
-oBox.onclick=toChange;
+	var oBox=document.getElementById('box');
+	oBox.onclick=toChange;
 }
